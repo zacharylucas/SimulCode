5fc5e806f530885a0fea766258fcd6573953b32a
Sectionalized <dinsmorezack@gmail.com>
2015-11-15 03:27:58 -0500

    Brought the old stuff back in

---
+++ WebAppHack/Scripts/CompileHelper.js
@@ -0,0 +1,19 @@
+﻿$(function (){
+    $("#sendDocData").click(function () {
+        var docData = $("#text_area_1").val();
+        alert(docData);
+        $.ajax({
+            url: "Compile",
+            type: 'POST',
+            data: { docDataIn: docData },
+            success: function (result) {
+                $("#text_area_result").val(result.compiled); 
+            },
+ 
+            error: function () {
+                alert("Something didn't quite work :(");
+            }
+        });
+    });
+});
+
